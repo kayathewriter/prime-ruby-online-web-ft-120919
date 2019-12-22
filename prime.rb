a2d2ad2d98267a9aabf08b5be79e95872a6b48d9
Mikael Kaya <kayaswork@gmail.com>
2019-12-22 09:10:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(x)
+  x = (1..40).to_a  
+ x.each {|x| if x <= 1 || (x > 3 && x % x == 0 && ( x % 2 == 0 || x % 3 == 0 ))
+ return false
+ else 
+ return true
+ end
+ }
+end
